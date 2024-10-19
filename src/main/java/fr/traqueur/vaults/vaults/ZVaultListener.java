2d7513f7c2fea97987905375cea5c9e7128e3e2b
Traqueur-dev <traqueur.dev@gmail.com>
2024-10-19 10:28:13 +0200

    feat (Click) : handle clicks in vault

--- src/main/java/fr/traqueur/vaults/vaults/ZVaultListener.java
+++ src/main/java/fr/traqueur/vaults/vaults/ZVaultListener.java
@@ -35,45 +35,97 @@ public class ZVaultListener implements Listener {
         Vault vault = menu.getVault();
         ClickType click = event.getClick();
         ItemStack cursor = event.getCursor() == null ? new ItemStack(Material.AIR) : event.getCursor().clone();
+        ItemStack current = event.getCurrentItem() == null ? new ItemStack(Material.AIR) : event.getCurrentItem().clone();
         int firstEmpty = inventory.firstEmpty();
         int correspondingSlot = -1;
         if(vault.isInfinite() && event.getRawSlot() <= vault.getSize()) {
             event.setCancelled(true);
-            if (firstEmpty == -1) {
-                return;
-            }
-            if(!cursor.getType().isAir()) {
-                correspondingSlot = inventory.first(cursor.getType());
-            }
-            if(correspondingSlot == -1) {
-                return;
-            }
         }
-
-        int slot = correspondingSlot == -1 ? firstEmpty : correspondingSlot;
 
         switch (click) {
             case RIGHT -> {
-                if(vault.isInfinite() && event.getRawSlot() <= vault.getSize()) {
-                    addToItem(inventory, slot, 1, player);
-                    if (cursor.getAmount() == 1) {
-                        event.getView().setCursor(new ItemStack(Material.AIR));
-                    } else {
-                        cursor.setAmount(cursor.getAmount() - 1);
-                        event.getView().setCursor(cursor);
+                if (vault.isInfinite()) {
+                    if(event.getRawSlot() <= vault.getSize()) {
+                        if(!cursor.getType().isAir()) {
+                            if(cursor.isSimilar(current)) {
+                                if(cursor.getAmount() >= cursor.getMaxStackSize()) {
+                                    return;
+                                }
+                                removeToItem(inventory, event.getRawSlot(), player, current);
+                                event.getView().setCursor(new ItemStack(current.getType(), cursor.getAmount() + 1));
+                                return;
+                            }
+
+                            correspondingSlot = inventory.first(cursor.getType());
+                            int slot = correspondingSlot == -1 ? firstEmpty : correspondingSlot;
+                            if (correspondingSlot == -1) {
+                                return;
+                            }
+                            addToItem(inventory, slot, 1, player);
+                            if (cursor.getAmount() == 1) {
+                                event.getView().setCursor(new ItemStack(Material.AIR));
+                            } else {
+                                cursor.setAmount(cursor.getAmount() - 1);
+                                event.getView().setCursor(cursor);
+                            }
+                        } else {
+                            if(!current.getType().isAir()) {
+                                removeToItem(inventory, event.getRawSlot(), player, current);
+                                event.getView().setCursor(new ItemStack(current.getType(), 1));
+                            }
+                        }
                     }
                 }
             }
             case LEFT -> {
+                if(!cursor.getType().isAir()) {
+                    correspondingSlot = inventory.first(cursor.getType());
+                }
+                int slot = correspondingSlot == -1 ? firstEmpty : correspondingSlot;
+                if (correspondingSlot == -1) {
+                    return;
+                }
                 if(vault.isInfinite() && event.getRawSlot() <= vault.getSize()) {
                     addToItem(inventory, slot, cursor.getAmount(), player);
                     event.getView().setCursor(new ItemStack(Material.AIR));
+                }
+            }
+            case SHIFT_LEFT, SHIFT_RIGHT -> {
+                if(vault.isInfinite() && event.getRawSlot() > vault.getSize()) {
+                    event.setCancelled(true);
+                    if(!current.getType().isAir()) {
+                        correspondingSlot = inventory.first(current.getType());
+                    }
+                    int slot = correspondingSlot == -1 ? firstEmpty : correspondingSlot;
+                    if (correspondingSlot == -1) {
+                        return;
+                    }
+                    addToItem(inventory, slot, current.getAmount(), player);
+                    event.setCurrentItem(new ItemStack(Material.AIR));
                 }
             }
             default -> {
                 player.sendMessage("Not implemented yet");
             }
         }
+    }
+
+    private void removeToItem(Inventory inventory, int slot, Player player, ItemStack current) {
+        int amount = this.getAmountFromItem(current);
+        if(amount == 1) {
+            inventory.setItem(slot, new ItemStack(Material.AIR));
+        } else {
+            VaultItem vaultItem = new VaultItem(current, amount - 1);
+            inventory.setItem(slot, vaultItem.toItem(player, true));
+        }
+    }
+
+    private int getAmountFromItem(ItemStack current) {
+        ItemMeta meta = current.getItemMeta();
+        if (meta == null) {
+            return 0;
+        }
+        return meta.getPersistentDataContainer().getOrDefault(vaultsManager.getAmountKey(), PersistentDataType.INTEGER, 0);
     }
 
     private void addToItem(Inventory inventory, int slot, int amount, Player player) {
